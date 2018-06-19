ad654ade53c5e76bcce29ea9f642b7b48ce04456
Juraj <jurajkulich@gmail.com>
2018-06-19 23:25:39 +0200

    Added ActivityRecyclerView

--- app/src/main/java/com/example/android/deprecure/AddItemToDiaryActivity.java
+++ app/src/main/java/com/example/android/deprecure/AddItemToDiaryActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.deprecure;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.widget.Toast;
 
 import com.example.android.deprecure.model.Mood;
 
@@ -12,20 +14,26 @@ import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class AddItemToDiaryActivity extends AppCompatActivity {
+public class AddItemToDiaryActivity extends AppCompatActivity implements MoodAdapter.OnClickListener, ActivityAdapter.OnClickListener{
 
     private ArrayList<Mood> moods = new ArrayList<>();
+    private ArrayList<String> activities = new ArrayList<>();
 
     @BindView(R.id.add_mood_recyclerview)
     RecyclerView mMoodRecyclerView;
 
+    @BindView(R.id.add_activity_recyclerview)
+    RecyclerView mActivityRecyclerView;
+
     private MoodAdapter moodAdapter;
+    private ActivityAdapter mActivityAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item_to_diary);
         ButterKnife.bind(this);
+
 
         moods.add(new Mood("Happy",
                 getResources().getIdentifier("happy1", "drawable", "com.example.android.deprecure")));
@@ -38,9 +46,44 @@ public class AddItemToDiaryActivity extends AppCompatActivity {
         moods.add(new Mood("Crying",
                 getResources().getIdentifier("crying", "drawable", "com.example.android.deprecure")));
 
-        mMoodRecyclerView.setLayoutManager(new StaggeredGridLayoutManager
-                (4, StaggeredGridLayoutManager.VERTICAL));
-        moodAdapter = new MoodAdapter(moods);
+        activities.add("Running");
+        activities.add("Working");
+        activities.add("Sleeping");
+        activities.add("Learning");
+        activities.add("Crying");
+        activities.add("Cycling");
+        activities.add("Playing football");
+
+        mMoodRecyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL) {
+            @Override
+            public boolean canScrollVertically() {
+                return false;
+            }
+        });
+        mActivityRecyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL) {
+                    @Override
+                    public boolean canScrollVertically() {
+                        return false;
+                    }
+                }
+        );
+        moodAdapter = new MoodAdapter(moods, this);
         mMoodRecyclerView.setAdapter(moodAdapter);
+
+        mActivityAdapter = new ActivityAdapter(activities, this);
+        mActivityRecyclerView.setAdapter(mActivityAdapter);
+
+    }
+
+    @Override
+    public void onItemClick(Mood mood) {
+        Toast.makeText(this, mood.getMoodName(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemClick(String activity) {
+        Toast.makeText(this, activity, Toast.LENGTH_SHORT).show();
     }
 }
