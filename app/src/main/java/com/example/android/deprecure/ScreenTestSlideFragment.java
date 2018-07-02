7eff9407e1b6af9aa0aa3ff8e74ef3ce70de44e8
yuro8 <jurajkulich@gmail.com>
2018-07-02 19:35:55 +0200

    Added RadioButtons

--- app/src/main/java/com/example/android/deprecure/ScreenTestSlideFragment.java
+++ app/src/main/java/com/example/android/deprecure/ScreenTestSlideFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -15,7 +17,7 @@ import butterknife.ButterKnife;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ScreenTestSlideFragment extends Fragment {
+public class ScreenTestSlideFragment extends Fragment implements View.OnClickListener {
 
     private String question;
 
@@ -34,12 +36,16 @@ public class ScreenTestSlideFragment extends Fragment {
         return slideFragment;
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_screen_test_slide, container, false);
         ButterKnife.bind(this, rootView);
+
+        rootView.findViewById(R.id.radio_not_at_all).setOnClickListener(this);
+        rootView.findViewById(R.id.radio_several_days).setOnClickListener(this);
+        rootView.findViewById(R.id.radio_more_than_half).setOnClickListener(this);
+        rootView.findViewById(R.id.radio_nearly_every_day).setOnClickListener(this);
 
         Bundle bundle = this.getArguments();
         if( bundle != null) {
@@ -50,4 +56,23 @@ public class ScreenTestSlideFragment extends Fragment {
         return rootView;
     }
 
+    @Override
+    public void onClick(View view) {
+
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.radio_not_at_all: {
+
+            }
+            case R.id.radio_several_days: {
+
+            }
+            case R.id.radio_more_than_half: {
+
+            }
+            case R.id.radio_nearly_every_day: {
+
+            }
+        }
+    }
 }
